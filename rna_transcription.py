f2b83878f0b04b77a2d92f4548f1cb4f8703033b
elizabethsotomayor <elizabethrsotomayor@gmail.com>
2021-04-22 21:04:53 -0500

    shorten code to include translation

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,15 +1,4 @@
 def to_rna(dna_strand):
-    rna_string = ''
-    for char in dna_strand:
-        if char == 'G':
-            rna_string += 'C'
-        elif char == 'C':
-            rna_string += 'G'
-        elif char == 'T':
-            rna_string += 'A'
-        elif char == 'A':
-            rna_string += 'U'
-        else:
-            break
-
+    translation = dna_strand.maketrans('GCTA', 'CGAU')
+    rna_string = dna_strand.translate(translation)
     return rna_string
